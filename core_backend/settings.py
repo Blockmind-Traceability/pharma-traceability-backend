cedcf96301bb8b72c5c7a97156aae0a70a9011df
edwinlopezz17 <edwinabdias@gmail.com>
2025-06-25 23:18:06 -0500

    MySQL dependenci resolved

--- core_backend/settings.py
+++ core_backend/settings.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
+import pymysql
+pymysql.install_as_MySQLdb()
 
 load_dotenv()  
 BASE_DIR = Path(__file__).resolve().parent.parent
